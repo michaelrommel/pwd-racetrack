3b1169092dd60e5394bc3e12debdf655f1193a98
Michael Rommel <rommel@layer-7.net>
2018-07-12 14:13:06 +0200

    Separate the db module directory from the binary databases dir

---
+++ bridge/db/index.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const level = require('level')
+
+var race = level('./databases/race', {valueEncoding: 'json'})
+var car = level('./databases/car', {valueEncoding: 'json'})
+var lane = level('./databases/lane', {valueEncoding: 'json'})
+var heat = level('./databases/heat', {valueEncoding: 'json'})
+var leaderboard = level('./databases/leaderboard', {valueEncoding: 'json'})
+var highscore = level('./databases/highscore', {valueEncoding: 'json'})
+var checkpoint = level('./databases/checkpoint', {valueEncoding: 'json'})
+
+module.exports = {
+  race: race, 
+  car: car, 
+  lane: lane, 
+  heat: heat, 
+  leaderboard: leaderboard, 
+  highscore: highscore,
+  checkpoint: checkpoint
+}
