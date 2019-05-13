0cc1d18c73881cd9615c645746fb23f2ff7a8d42
Michael Rommel <rommel@layer-7.net>
2019-05-13 08:55:16 +0000

    Add removal of websocket clients on disconnect.
    Patched the CSS files for proper display of the RaceID modal
    dialog. There is now a 'end' handler for disconnecting websocket
    clients.

--- bridge/network/routes/websocket/wsUtils.js
+++ bridge/network/routes/websocket/wsUtils.js
@@ -10,12 +10,26 @@ function clientAccept (req, res) {
   let upgrade = res.claimUpgrade()
   let clientConn = ws.accept(req, upgrade.socket, upgrade.head)
   logger.debug('%s::clientAccept: client accepted', MODULE_ID)
+  // store watershed connection into an array of clients
   clients.push(clientConn)
+  // set up functions for incoming data
+  clientConn.on('text', (t) => {
+    logger.debug('%s::clientAccept: data received: %s', MODULE_ID, t)
+  })
+  // remove ended connections
+  clientConn.on('end', (t) => {
+    logger.debug('%s::clientAccept: connection closed: %s', MODULE_ID, clientConn._remote)
+    for (let i = 0; i < clients.length; i++) {
+      if (clients[i] === clientConn) {
+        clients.splice(i, 1)
+      }
+    }
+  })
 }
 
 function notify () {
   clients.forEach((c) => {
-    logger.debug('%s::notify: client %s notified', MODULE_ID, c)
+    logger.debug('%s::notify: client %s notified', MODULE_ID, c._remote)
     c.send('test')
   })
 }
